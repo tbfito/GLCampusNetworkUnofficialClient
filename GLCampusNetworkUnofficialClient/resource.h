5e7f91be3752f1ec8cfbe24a46158385ca7b0944
iedon <admin@iedon.com>
2018-11-28 23:19:54 +0800

    添加项目文件。

---
+++ GLCampusNetworkUnofficialClient/resource.h
@@ -0,0 +1,27 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 GLCampusNetworkUnofficialClient.rc 使用
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_GLCAMPUSNETWORKUNOFFICIALCLIENT_DIALOG 102
+#define IDR_MAINFRAME                   128
+#define IDC_EDIT1                       1000
+#define IDC_EDIT2                       1001
+#define IDC_LIST1                       1002
+#define IDC_CHECK1                      1003
+#define IDC_CHECK2                      1004
+#define IDC_CHECK3                      1005
+#define IDC_CHECK4                      1006
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1004
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
